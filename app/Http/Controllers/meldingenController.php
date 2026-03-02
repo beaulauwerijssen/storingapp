c318b3920ec39d13059dfaefdd965d151f970a01
beaulauwerijssen <bjl@ziggo.nl>
2026-03-02 11:45:21 +0100

    Update meldingenController.php

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -1,22 +1,28 @@
 <?php
-
-//Variabelen vullen
 $attractie = $_POST['attractie'];
+$type = $_POST['type'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
+$overige_info = $_POST['overige_info'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
-
-//1. Verbinding
+$prioriteit = isset($_POST['prioriteit']) ? 1 : 0;
+echo $attractie . " / " . $type . " / " . $capaciteit . " / " . $melder;
 require_once '../../../config/conn.php';
 
-//2. Query
-$query = "INSERT INTO meldingen (attractie, type, melder) VALUES(:attractie, :type, :melder)";
-//3. Prepare
+$query = "INSERT INTO meldingen 
+          (attractie, type, prioriteit, capaciteit, melder, overige_info) 
+          VALUES 
+          (:attractie, :type, :prioriteit, :capaciteit, :melder, :overige_info)";
+
 $statement = $conn->prepare($query);
-//4. Execute
+
 $statement->execute([
     ':attractie' => $attractie,
-    ':type' => $capaciteit,
-    ':melder' => $melder
+    ':type' => $type,
+    ':prioriteit' => $prioriteit,
+    ':capaciteit' => $capaciteit,
+    ':melder' => $melder,
+    ':overige_info' => $overige_info
 ]);
+
+echo "<br>Melding succesvol opgeslagen!";
