0b5c6f1a14815db5c2b24e136271172fd72b46f0
beaulauwerijssen <bjl@ziggo.nl>
2026-03-02 11:38:32 +0100

    nhj

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -11,7 +11,12 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, type) VALUES(:attractie, :type)";
+$query = "INSERT INTO meldingen (attractie, type, melder) VALUES(:attractie, :type, :melder)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
 //4. Execute
+$statement->execute([
+    ':attractie' => $attractie,
+    ':type' => $capaciteit,
+    ':melder' => $melder
+]);
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -18,19 +18,39 @@
 
             <div class="form-group">
                 <label for="attractie">Naam attractie:</label>
-                <input type="text" name="attractie" id="attractie" class="form-input">
+                <input type="text" name="attractie" id="attractie" class="form-input" required>
             </div>
+
             <div class="form-group">
-                <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
+                <label for="type">Type:</label>
+                <select id="type" name="type" class="form-input" required>
+                    <option value="">- Kies een optie -</option>
+                    <option value="achtbaan">Achtbaan</option>
+                    <option value="waterglijbaan">Waterglijbaan</option>
+                    <option value="waterpark">Waterpark</option>
+                </select>
             </div>
+
+            <div class="form-group">
+                <label>
+                    <input type="checkbox" name="prioriteit" value="1">
+                    Hoge prioriteit
+                </label>
+            </div>
+
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
+
             <div class="form-group">
                 <label for="melder">Naam melder:</label>
-                <input type="text" name="melder" id="melder" class="form-input">
+                <input type="text" name="melder" id="melder" class="form-input" required>
+            </div>
+
+            <div class="form-group">
+                <label for="overige_info">Overige informatie:</label>
+                <textarea name="overige_info" id="overige_info" class="form-input" rows="4"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
