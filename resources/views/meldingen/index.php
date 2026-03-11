7e6074bd57adc1812e37d19a28d775883c99bd1c
beaulauwerijssen <bjl@ziggo.nl>
2026-03-11 09:48:19 +0100

    Update index.php

--- resources/views/meldingen/index.php
+++ resources/views/meldingen/index.php
@@ -21,23 +21,39 @@
         } ?>
 
         <div style="height: 300px; background: #ededed; display: flex; justify-content: center; align-items: center; color: #666666;">
-        <?php
-            require_once '../../../config/conn.php';
-            $query = "SELECT * FROM meldingen";
-            $statement = $conn->prepare($query);
-            $statement->execute();
-            $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
-        ?>
-        <?php foreach($meldingen as $item):?>
-        <div>
-            <p><?php echo $item['attractie'];?></p>
-            <p><?php echo $item['type'];?></p>
-            <p><?php echo $item['prioriteit'];?></p>
-            <p><?php echo $item['capaciteit'];?></p>
-            <p><?php echo $item['melder'];?></p>
-            <p><?php echo $item['overige_info'];?></p>
-        </div>
-        <?php endforeach; ?>
+        <table class="meldingen-table">
+            <thead>
+                <tr>
+                    <th>Attractie</th>
+                    <th>Type</th>
+                    <th>Prioriteit</th>
+                    <th>Capaciteit</th>
+                    <th>Melder</th>
+                    <th>Overige_info</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                    require_once '../../../config/conn.php';
+                    $query = "SELECT * FROM meldingen";
+                    $statement = $conn->prepare($query);
+                    $statement->execute();
+                    $meldingen = $statement->fetchAll(PDO::FETCH_ASSOC);
+                ?>
+                <?php foreach($meldingen as $item):?>
+                <div>
+                <tr>
+                    <td><?php echo $item['attractie'];?></td>
+                    <td><?php echo $item['type'];?></td>
+                    <td><?php echo $item['prioriteit'];?></td>
+                    <td><?php echo $item['capaciteit'];?></td>
+                    <td><?php echo $item['melder'];?></td>
+                    <td><?php echo $item['overige_info'];?></td>
+                </tr>
+                </div>
+                <?php endforeach; ?>
+            </tbody>
         </div>
     </div>
 
